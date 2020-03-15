69a2846fd26db5a872a0b2ec3ec2d2c83c4542bf
simple <2309780282@qq.com>
2020-03-15 13:02:20 +0800

    解决github警号

---
+++ front/blog/src/views/common/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="markdown-body">
+    <mavon-editor class="md" v-model="article"></mavon-editor>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      article: ""
+    };
+  },
+  watch: {
+    article: function() {
+      console.log(this.article);
+    }
+  },
+  methods: {},
+  mounted() {},
+  components: {}
+};
+</script>
